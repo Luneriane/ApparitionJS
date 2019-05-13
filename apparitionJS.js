0afc839907e37d91906463da57c696850f53df44
Luneriane <luneriane@gmail.com>
2019-05-14 01:07:36 +0200

    fini !

--- apparitionJS.js
+++ apparitionJS.js
@@ -1,57 +1,19 @@
-function myAnim(menuX) {
-    var letter = menuX.charAt(4),
-        mySection = "section" + letter;
+function myAnim(sectionX) {
+    var letter = sectionX.charAt(8),
+        myTitle = "#title" + letter,
+        myContent = "#content" + letter,
+        tl = new TimelineMax();
 
-        console.log(mySection);
-
-    TweenMax.to(menuX, 2, {
+    tl.to(sectionX, 2, {    //Conteneur de la section
         width: "100%",
         height: "100%",
-        zIndex: 4,
-        opacity: 1
-    });
-    TweenMax.to(menuX, 2, {
-        opacity: 0,
-        ease:Power4.easeInOut, 
-        delay: 2
-    });
-    TweenMax.to(mySection, 2, {
-        display: "block",
-        opacity: 0,
-        delay: 2
-    });
-    TweenMax.to(mySection, 2, {
-        opacity: 1,
-        ease:Power4.easeInOut,
-        delay: 2
-    });
+        zIndex: 4
+    })
+      .to(myTitle, 2, {    //Titre de la section
+        marginTop: "1%",
+        paddingBottom: "1em"
+        }, "-=2",)
+      .to(myContent, 3, {    //Contenu de la section
+        height: "95%"
+        }, "-=1.5")
 }
-
-
-/*function animMenu() {
-    0% {
-        menu {
-            width: 50%;
-            height: 50%;
-            z-index: 3;
-        }
-    }
-    75% {
-        menu {
-            width: 100%;
-            height: 100%;
-            z-index: 4;
-        }
-        section {
-            display: block;
-            z-index: 5
-        }
-    }
-    100% {
-        menu {
-            width: 50%;
-            height: 50%;
-            z-index: 10;
-        }
-    }
-} */
